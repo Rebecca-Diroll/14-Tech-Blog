d2c2d6a18c253dfbec4de9bc8e07a57dd6800118
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-05-12 09:54:52 -0400

    Update index.js in models

--- models/index.js
+++ models/index.js
@@ -1,5 +1,13 @@
 const User = require('./User');
-const Post = require('./Post');
+const Blog = require('./Blog');
 
+User.hasMany(Blog, {
+    foreignKey: 'user_id', 
+    onDelete: 'CASCADE'
+});
 
-module.exports = { User, Post };
+Blog.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, Blog };
