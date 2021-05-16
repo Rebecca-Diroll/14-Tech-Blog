2fb51392cf705b6cba071f87967ead29b4cf9cb4
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-05-16 14:22:28 -0400

    Add code to create profile in profile.js

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,3 +1,26 @@
 const newFormHandler = async (event) => {
     event.preventDefault();
-}
+
+    const title = document.querySelector("#book-title").value.trim();
+    const author = document.querySelector("#book-title").value.trim();
+    const description = document.querySelector("#book-title").value.trim();
+    const user_email = document.querySelector("#book-title").value.trim();
+
+    if (title && author && description && user_email) {
+        const response = await fetch(`/api/blogs`, {
+            method: "POST",
+            body: JSON.stringify({ title, author, description, user_email }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        if (response.ok) {
+            document.location.replace("/profile");
+        } else {
+            response.json(err);
+            alert("Failed to create blog.")
+        }
+    }
+};
+
